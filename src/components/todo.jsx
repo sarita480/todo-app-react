c524e6d2e64da3363fdbeb440bc3544fc799883e
sarita <adhsarita24@gmail.com>
2023-02-19 13:43:16 -0800

    add todo component

---
+++ src/components/todo.jsx
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+
+class TodoApp extends Component
+{
+    render()
+    {
+        return(
+            <div className="TodoApp">This is my ToodoAPp.</div>
+        )
+    }
+}
+export default TodoApp
